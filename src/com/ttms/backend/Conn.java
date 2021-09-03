8d44fd3967853cbff8575fbe2e2c33165d68c879
Rushi <hrushikeshborakhede1089@gmail.com>
2021-09-03 22:01:44 +0530

    Create Conn.java

---
+++ src/com/ttms/backend/Conn.java
@@ -0,0 +1,21 @@
+
+package com.ttms.backend;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c = DriverManager.getConnection("jdbc:mysql:///ttms","username","password");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}
