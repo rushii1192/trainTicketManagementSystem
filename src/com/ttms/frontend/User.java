e9255ff1bf2317f5c02fb07901f1f57aca5c7ecd
Rushi <hrushikeshborakhede1089@gmail.com>
2021-10-26 15:47:43 +0530

    Update User.java

--- src/com/ttms/frontend/User.java
+++ src/com/ttms/frontend/User.java
@@ -234,7 +234,7 @@ public class User extends javax.swing.JPanel {
                                         .addComponent(jLabel6))
                                     .addComponent(usr_name, javax.swing.GroupLayout.PREFERRED_SIZE, 340, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(162, 162, 162)))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(search_btn, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(add_btn, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
