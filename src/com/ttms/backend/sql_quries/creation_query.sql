d0a90368a3cfdf8145597f3fa52a0260b22cf6b7
Rushi <hrushikeshborakhede1089@gmail.com>
2021-11-09 16:53:33 +0530

    Update creation_query.sql

--- src/com/ttms/backend/sql_quries/creation_query.sql
+++ src/com/ttms/backend/sql_quries/creation_query.sql
@@ -1,4 +1,4 @@
-
+drop database if exists ttms;
 /*Creating database ttms*/
 create database ttms;
 
@@ -7,16 +7,17 @@ create table ttms.traindata (
     TrainNo	int,	
     TrainName varchar(100),
     StationName	varchar(500),
-    ArrivalTime datetime,
-    DepartureTime datetime,
-    Date date,
     DistanceFromSourceStation int,	
-    VacancyQuota varchar(100)
+    VacancyQuota varchar(100),
+    ArrivalTime varchar(100),
+    DepartureTime varchar(100),
+    Date varchar(100),
+    SourceStationName varchar(500)
 );
 
 /*Creating table UserData*/
 create table ttms.userdata (
-	UserId	varchar(50),
+    UserId	varchar(50),
     FirstName	varchar(100),
     LasttName	varchar(100),
     Address	varchar(500),
@@ -25,7 +26,7 @@ create table ttms.userdata (
     Role	varchar(50) default "User",
     ValletBalance int,
     Gender varchar(50),
-    DOB date,
+    DOB varchar(100),
     AadharNo varchar(16),
     Mobile varchar(10),
     primary key (UserId)
@@ -33,30 +34,30 @@ create table ttms.userdata (
 
 /*Creating table PassengerData*/
 create table ttms.passenger (
-	FirstName	varchar(100),
+    FirstName	varchar(100),
     LasttName	varchar(100),
-    Age	int,
-    Birthdate	date,
-    Aadhar	int,
+    Age	varchar(100),
+    Birthdate	varchar(100),
+    Aadhar	varchar(100),
     Gender	varchar(50),
-    MobileNo int
+    MobileNo varchar(100)
 );
 
 /*Creating table TicketData*/
 create table ttms.ticketdata (
-	TicketNo	int,
-    TrainNumber	int,
+    TicketNo	varchar(100),
+    TrainNumber	varchar(100),
     TrainName	varchar(100),
     SourceStation	varchar(100),
     DestinationStation	varchar(100),
     PassengerName varchar(100),
     SeatNo	varchar(20),
-    Price int,
+    Price varchar(100),
     primary key (TicketNo)
 );
 
 /*Creating table UserLogin*/
 create table ttms.userlogin (
-	Username varchar(100),
+    Username varchar(100),
     Password varchar(100)
 );
