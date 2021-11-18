97a4c6b942dc0d4632bf0fc14b79091f0b15dcb2
Rushi <hrushikeshborakhede1089@gmail.com>
2021-11-18 12:08:57 +0530

    Update Validation.java

--- src/com/ttms/backend/Validation.java
+++ src/com/ttms/backend/Validation.java
@@ -25,24 +25,24 @@ class InvalidPassword extends Exception{
 public class Validation {
     public void validity(String p) throws InvalidPassword{
 		if (p.length()<8) {
-		throw new InvalidPassword(1);
+                    throw new InvalidPassword(1);
 		}
 		if(true) {
 			int flag=0;
 			for (int i=0;i<=9;i++) {
                             String d=Integer.toString(i);
-				if(p.contains(d)) {
-                                    flag=1;
-				}
+                            if(p.contains(d)) {
+                                flag=1;
+                            }
 			}
 			if(flag==0) {
-				throw new InvalidPassword(2);
+                            throw new InvalidPassword(2);
 			}
 		}
 		Pattern check=Pattern.compile("[^a-z0-9]",Pattern.CASE_INSENSITIVE);
 		
 		if(!check.matcher(p).find()) {
-			throw new InvalidPassword(3);
+                    throw new InvalidPassword(3);
 		}
     }
     public boolean passwordChecker(String one,String two){
